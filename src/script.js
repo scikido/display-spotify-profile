f99eae2ae814c8b36197f47e25d3942ac40aefb8
scikido <sarvadnyachavhan07@outlook.com>
2023-04-05 18:30:52 +0530

    changed cb uri's

--- src/script.js
+++ src/script.js
@@ -19,7 +19,10 @@ export async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append(
+    "redirect_uri",
+    "https://display-spotify-profile-777.vercel.app/callback"
+  );
   params.append("scope", "user-read-private user-read-email");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
@@ -54,7 +57,10 @@ export async function getAccessToken(clientId, code) {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append(
+    "redirect_uri",
+    "https://display-spotify-profile-777.vercel.app/callback"
+  );
   params.append("code_verifier", verifier);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {
